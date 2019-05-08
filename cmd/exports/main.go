6a082862467cdde3dc6c3651bf0c64a4a842034b
Travis <tlongoria@salesforce.com>
2019-05-08 15:34:34 -0600

    WIP

--- cmd/exports/main.go
+++ cmd/exports/main.go
@@ -4,24 +4,26 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/buildpack/libbuildpack/logger"
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		fmt.Println("Usage:", os.Args[0], "EXPORTS_PATH", "PLATFORM_DIR")
 		return
 	}
 
-	log, err := logger.DefaultLogger(os.Args[3])
+	exportsPath := os.Args[1]
+	platformDir := os.Args[2]
+
+	log, err := logger.DefaultLogger(platformDir)
 	if err != nil {
 		log.Info(err.Error())
 		os.Exit(1)
 	}
-
-	exportsPath := os.Args[1]
 
 	data, err := ioutil.ReadFile(exportsPath)
 	if err != nil {
@@ -33,7 +35,18 @@ func main() {
 	lines := strings.SplitN(contents, "\n", 2)
 
 	for _, line := range lines {
+		fmt.Println(line)
 		components := strings.Split(line, "=")
-		ioutil.WriteFile(components[0], []byte(components[1]), 0644)
+		export := strings.Split(components[0], " ")
+		if strings.TrimSpace(export[0]) == "export" {
+			fmt.Println("Export 1:")
+			fmt.Println(export[1])
+			file := filepath.Join(platformDir, strings.TrimSpace(export[1]))
+			err := ioutil.WriteFile(file, []byte(components[1]), 0644)
+			if err != nil {
+				log.Info(err.Error())
+				os.Exit(3)
+			}
+		}
 	}
 }
