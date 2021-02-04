5179713cb7400a2b61e9e6bc3396ea1e4983f69b
Kamal Nasser <kamal@digitalocean.com>
2021-02-04 19:28:43 +0200

    eval export scripts

--- exports_test.go
+++ exports_test.go
@@ -1,103 +1,86 @@
 package cnbshim_test
 
 import (
+	"fmt"
 	"io/ioutil"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/heroku/cnb-shim"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestWriteEnvFile(t *testing.T) {
-	envDir, err := ioutil.TempDir("", "env")
-	if err != nil {
-		t.Error(err.Error())
+func TestExports(t *testing.T) {
+	tcs := []struct {
+		name     string
+		environ  []string // optional environ for the test env
+		wantEnvs map[string]string
+	}{
+		{
+			name: "export_simple",
+			wantEnvs: map[string]string{
+				"FOO": "bar",
+			},
+		},
+		{
+			name: "export_quotes",
+			wantEnvs: map[string]string{
+				"FOO": `b"a"r`,
+			},
+		},
+		{
+			name: "export_script",
+			environ: []string{
+				"PATH=/usr/bin:/bin:/custom",
+			},
+			wantEnvs: map[string]string{
+				"PATH":     "/some-path:/another:/usr/bin:/bin:/custom:/end",
+				"SOME_VAR": "yes",
+				"SQUARE_1": "1",
+				"SQUARE_2": "4",
+				"SQUARE_3": "9",
+			},
+		},
 	}
 
-	expectedFilename := "FOO"
-	expected := "bar"
-	err = cnbshim.WriteEnvFile(envDir, expectedFilename, expected)
-	if err != nil {
-		t.Error(err.Error())
-	}
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			tmp, err := ioutil.TempDir("", "exports")
+			require.NoError(t, err)
 
-	expectedFile := filepath.Join(envDir, expectedFilename)
-	actual, err := ioutil.ReadFile(expectedFile)
-	if err != nil {
-		t.Error(err.Error())
-	}
+			cmd := exec.Command("bin/exports", fmt.Sprintf("test/fixtures/%s/export", tc.name), ".", tmp)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			cmd.Env = tc.environ
+			err = cmd.Run()
+			require.NoError(t, err)
 
-	if string(actual) != expected {
-		t.Errorf("Expected %s; got %s", expected, actual)
-	}
-}
+			gotEnvs := make(map[string]string)
+			err = filepath.Walk(tmp, func(path string, info os.FileInfo, err error) error {
+				if info.IsDir() {
+					if path == tmp {
+						return nil
+					}
 
-func TestDumpExportsFile(t *testing.T) {
-	envDir, err := ioutil.TempDir("", "env")
+					return fmt.Errorf("unexpected directory %s", path)
+				}
 
-	exports := `
-export FOO=bar
-`
-	err = cnbshim.DumpExportsFile(exports, envDir)
-	if err != nil {
-		t.Error(err.Error())
-	}
+				name := strings.SplitN(info.Name(), ".", 2)
+				require.Len(t, name, 2)
+				assert.Equal(t, "override", name[1])
 
-	expectedFile := filepath.Join(envDir, "FOO")
-	expected := "bar"
-	actual, err := ioutil.ReadFile(expectedFile)
-	if err != nil {
-		t.Error(err.Error())
-	}
+				value, err := ioutil.ReadFile(path)
+				require.NoError(t, err)
 
-	if strings.TrimSpace(string(actual)) != expected {
-		t.Errorf("Expected %s; got %s", expected, actual)
-	}
-}
+				gotEnvs[name[0]] = string(value)
 
-func TestDumpExportsFileWithQuotes(t *testing.T) {
-	envDir, err := ioutil.TempDir("", "env")
-
-	exports := `
-export FOO="bar"
-`
-	err = cnbshim.DumpExportsFile(exports, envDir)
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	expectedFile := filepath.Join(envDir, "FOO")
-	expected := "bar"
-	actual, err := ioutil.ReadFile(expectedFile)
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	if strings.TrimSpace(string(actual)) != expected {
-		t.Errorf("Expected %s; got %s", expected, actual)
-	}
-}
-
-func TestDumpExportsFileWithEscapedQuotes(t *testing.T) {
-	envDir, err := ioutil.TempDir("", "env")
-
-	exports := `
-export FOO="b\"a\"r"
-`
-	err = cnbshim.DumpExportsFile(exports, envDir)
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	expectedFile := filepath.Join(envDir, "FOO")
-	expected := "b\\\"a\\\"r"
-	actual, err := ioutil.ReadFile(expectedFile)
-	if err != nil {
-		t.Error(err.Error())
-	}
-
-	if strings.TrimSpace(string(actual)) != expected {
-		t.Errorf("Expected %s; got %s", expected, actual)
+				return nil
+			})
+			require.NoError(t, err)
+			assert.Equal(t, tc.wantEnvs, gotEnvs)
+		})
 	}
 }
